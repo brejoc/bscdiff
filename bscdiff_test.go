52967c6c51e2358c0a29b40c91c6b8f3312b8215
Jochen Breuer <brejoc@gmail.com>
2019-11-19 21:08:36 +0100

    Adds test to find missing bscs in search results

--- bscdiff_test.go
+++ bscdiff_test.go
@@ -31,3 +31,43 @@ func TestPrettyPrintMissingBscs(t *testing.T) {
 		t.Errorf("got \"%s\", wanted \"%s\"", got, want)
 	}
 }
+
+func TestFindMissingBsc(t *testing.T) {
+	var change1 []searchResult
+	var change2 []searchResult
+
+	change1 = append(change1,
+		searchResult{
+			line:  1,
+			match: []string{"bsc#123"},
+			text:  "Line in text with bsc#123",
+		},
+	)
+	change1 = append(change1,
+		searchResult{
+			line:  2,
+			match: []string{"bsc#321"},
+			text:  "Line in text with bsc#321",
+		},
+	)
+
+	change2 = append(change2,
+		searchResult{
+			line:  1,
+			match: []string{"bsc#123"},
+			text:  "Line in text with bsc#123",
+		},
+	)
+	change2 = append(change2,
+		searchResult{
+			line:  2,
+			match: []string{"bsc#666"},
+			text:  "Line in text with bsc#666",
+		},
+	)
+	got := findMissingBsc(change1, change2)
+	want := []string{"bsc#321"}
+	if got[0] != want[0] {
+		t.Errorf("Got \"%s\", wanted \"%s\"", got, want)
+	}
+}
